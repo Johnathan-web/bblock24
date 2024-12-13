1b636b7c8e4e0650413659bbde808c912236fca3
Sharon Marzette <sharonmarzette@Sharons-MacBook-Air.local>
2024-12-13 18:01:45 -0500

    initial commit

--- block24/src/App.jsx
+++ block24/src/App.jsx
@@ -1,23 +1,49 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
+import './path-to-css.css'
 import './App.css'
 
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
-
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+ 
+  console.log(featuredPup)
   console.log("puppyList: ", puppyList);
-
+  function handleClick() {
+    
+  }
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
-    </div>
+{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+      {featPupId && <p>Selected Puppy ID: {featPupId}</p>}
+    {puppies.map((puppy) => {
+      return (
+        <p 
+          onClick={() => {console.log("puppy id: ", puppy.id); 
+            setFeatPupId(puppy.id); }} 
+          key={puppy.id}
+        >
+          {puppy.name}
+        </p>
+      );
+      
+    })}
+  </div>
   );
 }
+  
+  
 
 
 export default App
